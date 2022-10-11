0fa2217d1c9750cabcdf49ff8769c2b314514714
kuan <cugkuan@163.com>
2022-10-11 15:36:51 +0800

    CanCancelUriRequestBuild 独立

--- cs/src/main/java/com/brightk/cs/common/CanCancelUriRequestBuild.java
+++ cs/src/main/java/com/brightk/cs/common/CanCancelUriRequestBuild.java
@@ -1,6 +1,8 @@
 package com.brightk.cs.common;
 
+import android.content.Context;
 import android.net.Uri;
+import android.os.Bundle;
 
 import com.brightk.cs.CS;
 import com.brightk.cs.core.CsService;
@@ -8,18 +10,79 @@ import com.brightk.cs.core.OnRequestResultListener;
 import com.brightk.cs.core.UriRequest;
 import com.brightk.cs.core.UriRespond;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  可以取消的请求构建,需要 {@link CsService} 实现 cancel
  */
-public class CanCancelUriRequestBuild extends UriRequestBuild {
+public class CanCancelUriRequestBuild  {
     private CsService service;
     private OnRequestResultListener listener;
+    private Uri.Builder uriBuilder;
+    private Uri uri;
+    private WeakReference<Context> context;
+    private Map<String, Object> params;
+
     public CanCancelUriRequestBuild(String url) {
-        super(url);
+        this.uri = Uri.parse(url);
     }
     public CanCancelUriRequestBuild(Uri uri) {
-        super(uri);
+        this.uri = uri;
     }
+    public CanCancelUriRequestBuild addQueryParams(String key, String value) {
+        if (uriBuilder == null) {
+            uriBuilder = uri.buildUpon();
+        }
+        uriBuilder.appendQueryParameter(key, value);
+        return this;
+    }
+
+    public CanCancelUriRequestBuild setContext(Context context) {
+        this.context = new WeakReference(context);
+        return this;
+    }
+
+    public CanCancelUriRequestBuild setAction(String action) {
+        addParam(UriRequest.REQUEST_PARAMS_KEY_ACTION, action);
+        return this;
+    }
+
+    public CanCancelUriRequestBuild setBundle(Bundle bundle) {
+        addParam(UriRequest.REQUEST_PARAMS_KEY_BUNDLE, bundle);
+        return this;
+    }
+
+    public CanCancelUriRequestBuild addParam(String key, Object value) {
+        if (params == null) {
+            params = new HashMap<>();
+        }
+        params.put(key, value);
+        return this;
+    }
+    public CanCancelUriRequestBuild putParams(Map<String, Object> params) {
+        if (this.params == null) {
+            this.params = params;
+        } else {
+            this.params.putAll(params);
+        }
+        return this;
+    }
+    protected UriRequest build() {
+        if (uriBuilder != null){
+            uri = uriBuilder.build();
+        }
+        Context c = null;
+        if (context != null){
+            c  = context.get();
+        }
+        UriRequest request = new UriRequest(c, uri);
+        request.putParams(params);
+        return request;
+    }
+
+
     public void cancel(){
         if (service != null){
             service.cancel(listener);
--- cs/src/main/java/com/brightk/cs/common/UriRequestBuild.java
+++ cs/src/main/java/com/brightk/cs/common/UriRequestBuild.java
@@ -1,28 +1,95 @@
 package com.brightk.cs.common;
 
+import android.content.Context;
 import android.net.Uri;
-import android.os.Handler;
+import android.os.Bundle;
 import android.os.Looper;
-import android.os.NetworkOnMainThreadException;
 
 import com.brightk.cs.CS;
+import com.brightk.cs.core.OnRequestResultListener;
 import com.brightk.cs.core.UriRequest;
 import com.brightk.cs.core.UriRespond;
 
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 请求参数构建
  */
-public class UriRequestBuild extends RequestBuild {
+public class UriRequestBuild  {
     //线程是否挂起
     private volatile boolean isWait = false;
+    private Uri.Builder uriBuilder;
+    private Uri uri;
+    private WeakReference<Context> context;
+    private Map<String, Object> params;
     public UriRequestBuild(String url) {
-        super(url);
+        this.uri = Uri.parse(url);
     }
     public UriRequestBuild(Uri uri) {
-        super(uri);
+        this.uri = uri;
     }
+
+    public UriRequestBuild addQueryParams(String key, String value) {
+        if (uriBuilder == null) {
+            uriBuilder = uri.buildUpon();
+        }
+        uriBuilder.appendQueryParameter(key, value);
+        return this;
+    }
+
+    public UriRequestBuild setContext(Context context) {
+        this.context = new WeakReference(context);
+        return this;
+    }
+
+    public UriRequestBuild setAction(String action) {
+        addParam(UriRequest.REQUEST_PARAMS_KEY_ACTION, action);
+        return this;
+    }
+
+    public UriRequestBuild setBundle(Bundle bundle) {
+        addParam(UriRequest.REQUEST_PARAMS_KEY_BUNDLE, bundle);
+        return this;
+    }
+
+    public UriRequestBuild addParam(String key, Object value) {
+        if (params == null) {
+            params = new HashMap<>();
+        }
+        params.put(key, value);
+        return this;
+    }
+    public UriRequestBuild putParams(Map<String, Object> params) {
+        if (this.params == null) {
+            this.params = params;
+        } else {
+            this.params.putAll(params);
+        }
+        return this;
+    }
+    protected UriRequest build() {
+        if (uriBuilder != null){
+            uri = uriBuilder.build();
+        }
+        Context c = null;
+        if (context != null){
+            c  = context.get();
+        }
+        UriRequest request = new UriRequest(c, uri);
+        request.putParams(params);
+        return request;
+    }
+
+    public void call(OnRequestResultListener listener) {
+        CS.call(build(), listener);
+    }
+    public void call() {
+        call(null);
+    }
+
     public UriRespond connect() {
         UriRequest request = build();
         AtomicReference<UriRespond> uriRespond = new AtomicReference<>();
@@ -35,7 +102,6 @@ public class UriRequestBuild extends RequestBuild {
                     } catch (IllegalMonitorStateException e) {
                     }
                 }
-                isWait = false;
             }
         });
         synchronized (request) {
